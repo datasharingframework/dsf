57e579706f2ad63f7f6a63ee89361b83b51b80cf
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2023-03-08 14:39:31 +0100

    remove pseudonymization module

--- dsf-pseudonymization/dsf-pseudonymization-medic/src/main/java/dev/dsf/pseudonymization/bloomfilter/RecordBloomFilterGenerator.java
+++
@@ -1,8 +0,0 @@
-package dev.dsf.pseudonymization.bloomfilter;
-
-import dev.dsf.mpi.client.Idat;
-
-public interface RecordBloomFilterGenerator
-{
-	RecordBloomFilter generate(Idat idat);
-}
--- dsf-pseudonymization/dsf-pseudonymization-medic/src/main/java/dev/dsf/pseudonymization/translation/ResultSetTranslator.java
+++
@@ -1,8 +0,0 @@
-package dev.dsf.pseudonymization.translation;
-
-import dev.dsf.openehr.model.structure.ResultSet;
-
-public interface ResultSetTranslator
-{
-	ResultSet translate(ResultSet resultSet);
-}
