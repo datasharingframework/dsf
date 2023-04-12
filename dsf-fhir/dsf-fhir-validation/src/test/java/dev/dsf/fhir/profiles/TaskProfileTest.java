7c85fc5130f65ccd4870372820e5dd95283b2ea4
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2023-04-12 20:30:18 +0200

    add business key constraint to task profile

---
+++ dsf-fhir/dsf-fhir-validation/src/test/java/dev/dsf/fhir/profiles/TaskProfileTest.java
@@ -0,0 +1,149 @@
+package dev.dsf.fhir.profiles;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+import org.hl7.fhir.r4.model.ResourceType;
+import org.hl7.fhir.r4.model.StringType;
+import org.hl7.fhir.r4.model.Task;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ca.uhn.fhir.validation.ResultSeverityEnum;
+import ca.uhn.fhir.validation.SingleValidationMessage;
+import ca.uhn.fhir.validation.ValidationResult;
+import dev.dsf.fhir.validation.ResourceValidator;
+import dev.dsf.fhir.validation.ResourceValidatorImpl;
+import dev.dsf.fhir.validation.ValidationSupportRule;
+
+public class TaskProfileTest
+{
+	private static final Logger logger = LoggerFactory.getLogger(TaskProfileTest.class);
+
+	@ClassRule
+	public static final ValidationSupportRule validationRule = new ValidationSupportRule(
+			List.of("dsf-task-base-0.5.0.xml"), List.of("dsf-bpmn-message-0.5.0.xml"),
+			List.of("dsf-bpmn-message-0.5.0.xml"));
+
+	private ResourceValidator resourceValidator = new ResourceValidatorImpl(validationRule.getFhirContext(),
+			validationRule.getValidationSupport());
+
+	@Test
+	public void testTaskValidRequestedWithoutBusinessKey()
+	{
+		Task task = createTask(Task.TaskStatus.REQUESTED);
+		testTaskValid(task);
+	}
+
+	@Test
+	public void testTaskValidRequestedWithBusinessKey()
+	{
+		Task task = createTaskWithBusinessKey(Task.TaskStatus.REQUESTED);
+		testTaskValid(task);
+	}
+
+	@Test
+	public void testTaskValidInProgressWithBusinessKey()
+	{
+		Task task = createTaskWithBusinessKey(Task.TaskStatus.INPROGRESS);
+		testTaskValid(task);
+	}
+
+	@Test
+	public void testTaskInvalidInProgressMissingBusinessKey()
+	{
+		Task task = createTask(Task.TaskStatus.INPROGRESS);
+		testTaskInvalidMissingBusinessKey(task);
+	}
+
+	@Test
+	public void testTaskValidCompletedWithBusinessKey()
+	{
+		Task task = createTaskWithBusinessKey(Task.TaskStatus.COMPLETED);
+		testTaskValid(task);
+	}
+
+	@Test
+	public void testTaskInvalidCompletedMissingBusinessKey()
+	{
+		Task task = createTask(Task.TaskStatus.COMPLETED);
+		testTaskInvalidMissingBusinessKey(task);
+	}
+
+	@Test
+	public void testTaskValidFailedWithBusinessKey()
+	{
+		Task task = createTaskWithBusinessKey(Task.TaskStatus.FAILED);
+		testTaskValid(task);
+	}
+
+	@Test
+	public void testTaskInvalidFailedMissingBusinessKey()
+	{
+		Task task = createTask(Task.TaskStatus.FAILED);
+		testTaskInvalidMissingBusinessKey(task);
+	}
+
+	private Task createTaskWithBusinessKey(Task.TaskStatus status)
+	{
+		Task task = createTask(status);
+		task.addInput().setValue(new StringType(UUID.randomUUID().toString())).getType().addCoding()
+				.setSystem("http://dsf.dev/fhir/CodeSystem/bpmn-message").setCode("business-key");
+
+		return task;
+	}
+
+	private Task createTask(Task.TaskStatus status)
+	{
+		Task task = new Task();
+		task.getMeta().addProfile("http://dsf.dev/fhir/StructureDefinition/task-base");
+		task.setInstantiatesUri("http://dsf.dev/bpe/Process/foo|0.1.0");
+		task.setStatus(status);
+		task.setIntent(Task.TaskIntent.ORDER);
+		task.setAuthoredOn(new Date());
+		task.getRequester().setType(ResourceType.Organization.name()).getIdentifier()
+				.setSystem("http://dsf.dev/sid/organization-identifier").setValue("Test_DIC");
+		task.getRestriction().addRecipient().setType(ResourceType.Organization.name()).getIdentifier()
+				.setSystem("http://dsf.dev/sid/organization-identifier").setValue("Test_DIC");
+
+		task.addInput().setValue(new StringType("message")).getType().addCoding()
+				.setSystem("http://dsf.dev/fhir/CodeSystem/bpmn-message").setCode("message-name");
+
+		return task;
+	}
+
+	private void testTaskValid(Task task)
+	{
+		ValidationResult result = validate(task);
+
+		assertEquals(0, result.getMessages().stream().filter(m -> ResultSeverityEnum.ERROR.equals(m.getSeverity())
+				|| ResultSeverityEnum.FATAL.equals(m.getSeverity())).count());
+	}
+
+	private void testTaskInvalidMissingBusinessKey(Task task)
+	{
+		ValidationResult result = validate(task);
+
+		assertEquals(1,
+				result.getMessages().stream()
+						.filter(m -> ResultSeverityEnum.ERROR.equals(m.getSeverity())
+								|| ResultSeverityEnum.FATAL.equals(m.getSeverity()))
+						.map(SingleValidationMessage::getMessage).filter(Objects::nonNull)
+						.filter(m -> m.contains("business-key-if-status-inprogress-completed-failed")).count());
+	}
+
+	private ValidationResult validate(Task task)
+	{
+		ValidationResult result = resourceValidator.validate(task);
+		result.getMessages().stream().map(m -> m.getLocationString() + " " + m.getLocationLine() + ":"
+				+ m.getLocationCol() + " - " + m.getSeverity() + ": " + m.getMessage()).forEach(logger::info);
+
+		return result;
+	}
+}
