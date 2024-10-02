4b8a60cfa3d11d2ee2d33a921da2cca7ea7a7043
Hauke Hund <hauke.hund@hs-heilbronn.de>
2024-10-02 19:43:24 +0200

    new all_read_access_resources view and improved trigger
    Added a new all_read_access_resources view that includes all resources
    except Task and QuestionnaireResponse.
    
    Improved after organizations and organization_affiliations insert
    triggers by removing unnecessary delete/insert operations for FHIR
    updates (db inserts) of unchanged Organization and
    OrganizationAffiliation resources. Unchanged in this case means no
    active status change for both resource types and no role change for
    OrganizationAffiliations. Both triggers now also use the new
    all_read_access_resources view.

--- dsf-fhir/dsf-fhir-server/src/main/resources/db/trigger_functions/on_organizations_insert.sql
+++ dsf-fhir/dsf-fhir-server/src/main/resources/db/trigger_functions/on_organizations_insert.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE FUNCTION on_organizations_insert() RETURNS TRIGGER AS $$
 DECLARE
+	organization_exists_active BOOLEAN := EXISTS (SELECT 1 FROM organizations WHERE organization_id = NEW.organization_id AND version = NEW.version - 1 AND deleted IS NULL AND organization->>'active' = 'true');
 	reference_regex TEXT := '((http|https):\/\/([A-Za-z0-9\-\\\.\:\%\$]*\/)+)?(Account|ActivityDefinition|AdverseEvent|AllergyIntolerance|Appointment|AppointmentResponse|AuditEvent|Basic|Binary|BiologicallyDerivedProduct|BodyStructure|Bundle|CapabilityStatement|CarePlan|CareTeam|CatalogEntry|ChargeItem|ChargeItemDefinition|Claim|ClaimResponse|ClinicalImpression|CodeSystem|Communication|CommunicationRequest|CompartmentDefinition|Composition|ConceptMap|Condition|Consent|Contract|Coverage|CoverageEligibilityRequest|CoverageEligibilityResponse|DetectedIssue|Device|DeviceDefinition|DeviceMetric|DeviceRequest|DeviceUseStatement|DiagnosticReport|DocumentManifest|DocumentReference|EffectEvidenceSynthesis|Encounter|Endpoint|EnrollmentRequest|EnrollmentResponse|EpisodeOfCare|EventDefinition|Evidence|EvidenceVariable|ExampleScenario|ExplanationOfBenefit|FamilyMemberHistory|Flag|Goal|GraphDefinition|Group|GuidanceResponse|HealthcareService|ImagingStudy|Immunization|ImmunizationEvaluation|ImmunizationRecommendation|ImplementationGuide|InsurancePlan|Invoice|Library|Linkage|List|Location|Measure|MeasureReport|Media|Medication|MedicationAdministration|MedicationDispense|MedicationKnowledge|MedicationRequest|MedicationStatement|MedicinalProduct|MedicinalProductAuthorization|MedicinalProductContraindication|MedicinalProductIndication|MedicinalProductIngredient|MedicinalProductInteraction|MedicinalProductManufactured|MedicinalProductPackaged|MedicinalProductPharmaceutical|MedicinalProductUndesirableEffect|MessageDefinition|MessageHeader|MolecularSequence|NamingSystem|NutritionOrder|Observation|ObservationDefinition|OperationDefinition|OperationOutcome|Organization|OrganizationAffiliation|Patient|PaymentNotice|PaymentReconciliation|Person|PlanDefinition|Practitioner|PractitionerRole|Procedure|Provenance|Questionnaire|QuestionnaireResponse|RelatedPerson|RequestGroup|ResearchDefinition|ResearchElementDefinition|ResearchStudy|ResearchSubject|RiskAssessment|RiskEvidenceSynthesis|Schedule|SearchParameter|ServiceRequest|Slot|Specimen|SpecimenDefinition|StructureDefinition|StructureMap|Subscription|Substance|SubstanceNucleicAcid|SubstancePolymer|SubstanceProtein|SubstanceReferenceInformation|SubstanceSourceMaterial|SubstanceSpecification|SupplyDelivery|SupplyRequest|Task|TerminologyCapabilities|TestReport|TestScript|ValueSet|VerificationResult|VisionPrescription)\/([A-Za-z0-9\-\.]{1,64})(\/_history\/([A-Za-z0-9\-\.]{1,64}))?';
 	organization_identifier TEXT := jsonb_path_query(NEW.organization, '$.identifier[*]?(@.system == "http://dsf.dev/sid/organization-identifier")')->>'value';
 	organization_insert_count INT;
@@ -9,36 +10,40 @@ DECLARE
 	roles_delete_count INT;
 BEGIN
 	PERFORM on_resources_insert(NEW.organization_id, NEW.version, NEW.organization);
-	
-	DELETE FROM read_access
-	WHERE access_type = 'ORGANIZATION'
-	AND organization_id = NEW.organization_id;
-	
-	GET DIAGNOSTICS delete_count = ROW_COUNT;
-	RAISE NOTICE 'Existing rows deleted from read_access for created/updated organization, ORGANIZATION Tag: %', delete_count;
-	
-	DELETE FROM read_access
-	WHERE access_type = 'ROLE'
-	AND organization_affiliation_id IN (
-		SELECT organization_affiliation_id FROM current_organization_affiliations 
-		WHERE NEW.organization_id = (regexp_match(organization_affiliation->'participatingOrganization'->>'reference', reference_regex))[5]::uuid
-		OR NEW.organization_id = (regexp_match(organization_affiliation->'organization'->>'reference', reference_regex))[5]::uuid
-	); 
 
-	GET DIAGNOSTICS roles_delete_count = ROW_COUNT;
-	RAISE NOTICE 'Existing rows deleted from read_access for created/updated organization, ROLE Tag: %', roles_delete_count;
-	
-	RAISE NOTICE 'NEW.organization->>''active'' = ''%''', NEW.organization->>'active';
-	IF (NEW.organization->>'active' = 'true') THEN
+	IF (NEW.organization->>'active' = 'false') AND organization_exists_active THEN
+		RAISE NOTICE 'new organization inactive and old organization exists and active -> delete';
+
+		DELETE FROM read_access
+		WHERE access_type = 'ORGANIZATION'
+		AND organization_id = NEW.organization_id;
+
+		GET DIAGNOSTICS delete_count = ROW_COUNT;
+		RAISE NOTICE 'Existing rows deleted from read_access for created/updated organization, ORGANIZATION Tag: %', delete_count;
+
+		DELETE FROM read_access
+		WHERE access_type = 'ROLE'
+		AND organization_affiliation_id IN (
+			SELECT organization_affiliation_id FROM current_organization_affiliations 
+			WHERE NEW.organization_id = (regexp_match(organization_affiliation->'participatingOrganization'->>'reference', reference_regex))[5]::uuid
+			OR NEW.organization_id = (regexp_match(organization_affiliation->'organization'->>'reference', reference_regex))[5]::uuid
+		); 
+
+		GET DIAGNOSTICS roles_delete_count = ROW_COUNT;
+		RAISE NOTICE 'Existing rows deleted from read_access for created/updated organization, ROLE Tag: %', roles_delete_count;
+
+	ELSIF (NEW.organization->>'active' = 'true') AND NOT organization_exists_active THEN
+		RAISE NOTICE 'new organization active and old organization not exist or inactive -> insert';
+		
 		INSERT INTO read_access
 			SELECT id, version, 'ORGANIZATION', NEW.organization_id, NULL
-			FROM all_resources
+			FROM all_read_access_resources
 			WHERE jsonb_path_exists(resource,('$.meta.tag[*] ? (@.code == "ORGANIZATION" && @.system == "http://dsf.dev/fhir/CodeSystem/read-access-tag")
 					.extension[*]?(@.url == "http://dsf.dev/fhir/StructureDefinition/extension-read-access-organization")
 					.valueIdentifier[*]?(@.system == "http://dsf.dev/sid/organization-identifier" && @.value == "' || organization_identifier || '")')::jsonpath);
-		
+
 		GET DIAGNOSTICS organization_insert_count = ROW_COUNT;
-		
+
 		WITH temp_role_ids AS (		
 		INSERT INTO read_access 			
 			SELECT r.id, r.version, 'ROLE', member_organization_id, organization_affiliation_id FROM (
@@ -72,7 +77,7 @@ BEGIN
 				WHERE parent_organization_organization_id = NEW.organization_id OR member_organization_id = NEW.organization_id
 				) AS oa
 				LEFT JOIN (
-					SELECT id, version, resource FROM all_resources
+					SELECT id, version, resource FROM all_read_access_resources
 				) AS r
 				ON r.resource->'meta'->'tag' @> 
 					('[{"extension":[{"url":"http://dsf.dev/fhir/StructureDefinition/extension-read-access-parent-organization-role","extension":[{"url":"parent-organization","valueIdentifier":{"system":"http://dsf.dev/sid/organization-identifier","value":"'
@@ -101,10 +106,8 @@ BEGIN
 
 		GET DIAGNOSTICS binary_insert_count = ROW_COUNT;
 		RAISE NOTICE 'Rows inserted into read_access based on Binary.securityContext: %', binary_insert_count;
-
-	ELSIF (NEW.organization->>'active' = 'false') THEN
-		RAISE NOTICE 'Not inserting any entries to read_access, created/updated organization is not active';
 	END IF;
+
 	RETURN NEW;
 END;
 $$ LANGUAGE PLPGSQL
