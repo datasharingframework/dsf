2c68e4721ac9682d55a117763ce90d64618c8fdd
Hauke Hund <hauke.hund@hs-heilbronn.de>
2025-01-21 18:54:38 +0100

    subject/issuer checks optional if auth not via client cert (oidc)
    If SSL_VERIFY_CLIENT is set to 'optional' the subject country and issuer
    common-name checks are only relevant if the user is authenticated via a
    client certificate.

--- dsf-docker/fhir_proxy/start.sh
+++ dsf-docker/fhir_proxy/start.sh
@@ -21,11 +21,18 @@ if ! is_comma_separated_list "$SSL_EXPECTED_CLIENT_I_DN_CN_VALUES"; then
   exit 1
 fi
 
+if [ "$SSL_VERIFY_CLIENT" != "optional" ] && [ "$SSL_VERIFY_CLIENT" != "require" ]; then
+    echo "Error: SSL_VERIFY_CLIENT must be set to either 'optional' or 'require'"
+    exit 1
+fi
+
 out="./conf/extra/certificate_require_expr.conf"
 
 if [ -e "$out" ]; then
   echo "Info: Not creating $out, file exists"
-else
+elif [ "$SSL_VERIFY_CLIENT" == "optional" ]; then
+  echo "Require expr \"%{SSL_CLIENT_VERIFY} == 'NONE' || %{SSL_CLIENT_S_DN_C} in { $SSL_EXPECTED_CLIENT_S_DN_C_VALUES } && %{SSL_CLIENT_I_DN_CN} in { $SSL_EXPECTED_CLIENT_I_DN_CN_VALUES }\"" > "$out"
+elif [ "$SSL_VERIFY_CLIENT" == "require" ]; then
   echo "Require expr \"%{SSL_CLIENT_S_DN_C} in { $SSL_EXPECTED_CLIENT_S_DN_C_VALUES } && %{SSL_CLIENT_I_DN_CN} in { $SSL_EXPECTED_CLIENT_I_DN_CN_VALUES }\"" > "$out"
 fi
 
