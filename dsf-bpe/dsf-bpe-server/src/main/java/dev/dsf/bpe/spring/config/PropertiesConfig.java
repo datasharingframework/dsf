4fd1d13f9c698ee01c2635a54a7666ec39643c41
Hauke Hund <hauke.hund@hs-heilbronn.de>
2025-02-17 11:06:56 +0100

    typo fix

--- dsf-bpe/dsf-bpe-server/src/main/java/dev/dsf/bpe/spring/config/PropertiesConfig.java
+++ dsf-bpe/dsf-bpe-server/src/main/java/dev/dsf/bpe/spring/config/PropertiesConfig.java
@@ -126,7 +126,7 @@ public class PropertiesConfig implements InitializingBean
 	private long websocketRetrySleepMillis;
 
 	@Documentation(description = "Directory containing the DSF BPE process plugins for deployment on startup of the DSF BPE server", recommendation = "Change only if you don't use the provided directory structure from the installation guide or made changes to tit")
-	@Value("${dev.dsf.bpe.process.plugin.directroy:process}")
+	@Value("${dev.dsf.bpe.process.plugin.directory:process}")
 	private String processPluginDirectory;
 
 	@Documentation(description = "List of process names that should be excluded from deployment during startup of the DSF BPE server; comma or space separated list, YAML block scalars supported", recommendation = "Only deploy processes that can be started depending on your organization's roles in the Allow-List", example = "dsfdev_updateAllowList|1.0, another_process|x.y")
