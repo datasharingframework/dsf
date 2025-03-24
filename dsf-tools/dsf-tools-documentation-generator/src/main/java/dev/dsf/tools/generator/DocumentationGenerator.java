fe65f2142b66d13fb9c1760de27a2781dc73dc2b
Hauke Hund <hauke.hund@hs-heilbronn.de>
2025-03-24 13:19:22 +0100

    .secret property special case, updated documentation oidc code flow
    * The DocumentationGenerator now generates "or ..._FILE" headings also
    for properties ending in .secret
    * Update the recommendation for
    dev.dsf.server.auth.oidc.authorization.code.flow to reflect the
    generator change.
    
    cherry-picked from DSF 1.7.1

--- dsf-tools/dsf-tools-documentation-generator/src/main/java/dev/dsf/tools/generator/DocumentationGenerator.java
+++ dsf-tools/dsf-tools-documentation-generator/src/main/java/dev/dsf/tools/generator/DocumentationGenerator.java
@@ -222,7 +222,7 @@ public class DocumentationGenerator extends AbstractMojo
 			String property = getDocumentationString("Property", initialProperty);
 
 			String initialEnvironment = initialProperty.replace(".", "_").toUpperCase();
-			String environment = initialProperty.endsWith(".password")
+			String environment = initialProperty.endsWith(".password") || initialProperty.endsWith(".secret")
 					? String.format("%s or %s_FILE", initialEnvironment, initialEnvironment)
 					: initialEnvironment;
 
@@ -261,7 +261,7 @@ public class DocumentationGenerator extends AbstractMojo
 			String property = getDocumentationString("Property", initialProperty);
 
 			String initialEnvironment = initialProperty.replace(".", "_").toUpperCase();
-			String environment = initialProperty.endsWith(".password")
+			String environment = initialProperty.endsWith(".password") || initialProperty.endsWith(".secret")
 					? String.format("%s or %s_FILE", initialEnvironment, initialEnvironment)
 					: initialEnvironment;
 
