06b5a0a3a88697f4df4846f92866b1cc25627c42
Hauke Hund <hauke.hund@hs-heilbronn.de>
2025-05-06 18:28:25 +0200

    added missing @Override annotations

--- dsf-bpe/dsf-bpe-process-api-v2/src/main/java/dev/dsf/bpe/v2/client/dsf/BinaryInputStream.java
+++ dsf-bpe/dsf-bpe-process-api-v2/src/main/java/dev/dsf/bpe/v2/client/dsf/BinaryInputStream.java
@@ -37,71 +37,85 @@ public class BinaryInputStream extends InputStream
 		return range;
 	}
 
+	@Override
 	public int read() throws IOException
 	{
 		return delegate.read();
 	}
 
+	@Override
 	public int read(byte[] b) throws IOException
 	{
 		return delegate.read(b);
 	}
 
+	@Override
 	public int read(byte[] b, int off, int len) throws IOException
 	{
 		return delegate.read(b, off, len);
 	}
 
+	@Override
 	public byte[] readAllBytes() throws IOException
 	{
 		return delegate.readAllBytes();
 	}
 
+	@Override
 	public byte[] readNBytes(int len) throws IOException
 	{
 		return delegate.readNBytes(len);
 	}
 
+	@Override
 	public int readNBytes(byte[] b, int off, int len) throws IOException
 	{
 		return delegate.readNBytes(b, off, len);
 	}
 
+	@Override
 	public long skip(long n) throws IOException
 	{
 		return delegate.skip(n);
 	}
 
+	@Override
 	public void skipNBytes(long n) throws IOException
 	{
 		delegate.skipNBytes(n);
 	}
 
+	@Override
 	public int available() throws IOException
 	{
 		return delegate.available();
 	}
 
+	@Override
 	public void close() throws IOException
 	{
 		delegate.close();
 	}
 
+	@Override
 	public void mark(int readlimit)
 	{
 		delegate.mark(readlimit);
 	}
 
+	@Override
 	public void reset() throws IOException
 	{
 		delegate.reset();
 	}
 
+	@Override
 	public boolean markSupported()
 	{
 		return delegate.markSupported();
 	}
 
+	@Override
 	public long transferTo(OutputStream out) throws IOException
 	{
 		return delegate.transferTo(out);
