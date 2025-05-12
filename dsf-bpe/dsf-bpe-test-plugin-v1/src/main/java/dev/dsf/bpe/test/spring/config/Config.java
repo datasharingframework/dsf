5dc7891e65645066319d079dcfbe575fa0f754c0
Hauke Hund <hauke.hund@hs-heilbronn.de>
2025-05-13 01:28:59 +0200

    support for v2 plugins, improved default value detection
    * added documentation annotations to bpe test plugins
    * added config for documentation-generator maven plugin to bpe test
    plugin modules

--- dsf-bpe/dsf-bpe-test-plugin-v1/src/main/java/dev/dsf/bpe/test/spring/config/Config.java
+++ dsf-bpe/dsf-bpe-test-plugin-v1/src/main/java/dev/dsf/bpe/test/spring/config/Config.java
@@ -17,13 +17,16 @@ import dev.dsf.bpe.test.service.OrganizationProviderTest;
 import dev.dsf.bpe.test.service.ProxyTest;
 import dev.dsf.bpe.test.service.TestActivitySelector;
 import dev.dsf.bpe.v1.ProcessPluginApi;
+import dev.dsf.bpe.v1.documentation.ProcessDocumentation;
 
 @Configuration
 public class Config implements InitializingBean
 {
+	@ProcessDocumentation(description = "Mandatory property", example = "foo", required = true)
 	@Value("${dev.dsf.bpe.test.env.mandatory:#{null}}")
 	private String envVariableMandatory;
 
+	@ProcessDocumentation(description = "Property with default value", recommendation = "Override default value if necessary")
 	@Value("${dev.dsf.bpe.test.env.optional:default-value}")
 	private String envVariableOptional;
 
--- dsf-tools/dsf-tools-documentation-generator/src/main/java/dev/dsf/tools/generator/DocumentationGenerator.java
+++ dsf-tools/dsf-tools-documentation-generator/src/main/java/dev/dsf/tools/generator/DocumentationGenerator.java
@@ -1,7 +1,5 @@
 package dev.dsf.tools.generator;
 
-import static java.util.stream.Collectors.toList;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,14 +13,19 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.ServiceLoader;
+import java.util.ServiceLoader.Provider;
 import java.util.Set;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -42,8 +45,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
-import dev.dsf.bpe.v1.ProcessPluginDefinition;
-import dev.dsf.bpe.v1.documentation.ProcessDocumentation;
 import dev.dsf.common.documentation.Documentation;
 
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.PREPARE_PACKAGE, requiresDependencyResolution = ResolutionScope.COMPILE)
@@ -53,6 +54,9 @@ public class DocumentationGenerator extends AbstractMojo
 
 	private static final String ENV_VARIABLE_PLACEHOLDER = "${env_variable}";
 	private static final String PROPERTY_NAME_PLACEHOLDER = " ${property_name}";
+
+	private static final Pattern DEFAULT_VALUE_PATTERN_1 = Pattern.compile("#\\{(.*)\\}");
+	private static final Pattern DEFAULT_VALUE_PATTERN_2 = Pattern.compile(".*\\$\\{(.*)\\}.*");
 
 	private record DocumentationEntry(String propertyName, String value)
 	{
@@ -85,16 +89,30 @@ public class DocumentationGenerator extends AbstractMojo
 
 		Set<Field> dsfFields = reflections.getFieldsAnnotatedWith(Documentation.class);
 		if (!dsfFields.isEmpty())
-		{
 			writeFields(dsfFields, dsfDocumentationGenerator(), file, workingPackage);
+
+		// v1
+		Set<Field> processFieldsV1 = reflections
+				.getFieldsAnnotatedWith(dev.dsf.bpe.v1.documentation.ProcessDocumentation.class);
+		if (!processFieldsV1.isEmpty())
+		{
+
+			List<String> pluginProcessNames = getPluginProcessNames(reflections,
+					dev.dsf.bpe.v1.ProcessPluginDefinition.class, classLoader, workingPackage, this::getV1ProcessNames);
+			writeFields(processFieldsV1, processDocumentationGenerator(pluginProcessNames, ProcessDocumentation::v1),
+					file, workingPackage);
 		}
 
-		// TODO add process API version abstraction
-		Set<Field> processFields = reflections.getFieldsAnnotatedWith(ProcessDocumentation.class);
-		if (!processFields.isEmpty())
+		// v2
+		Set<Field> processFieldsV2 = reflections
+				.getFieldsAnnotatedWith(dev.dsf.bpe.v2.documentation.ProcessDocumentation.class);
+		if (!processFieldsV2.isEmpty())
 		{
-			List<String> pluginProcessNames = getPluginProcessNames(reflections, classLoader, workingPackage);
-			writeFields(processFields, processDocumentationGenerator(pluginProcessNames), file, workingPackage);
+
+			List<String> pluginProcessNames = getPluginProcessNames(reflections,
+					dev.dsf.bpe.v2.ProcessPluginDefinition.class, classLoader, workingPackage, this::getV2ProcessNames);
+			writeFields(processFieldsV2, processDocumentationGenerator(pluginProcessNames, ProcessDocumentation::v2),
+					file, workingPackage);
 		}
 	}
 
@@ -127,51 +145,65 @@ public class DocumentationGenerator extends AbstractMojo
 		}
 	}
 
-	private List<String> getPluginProcessNames(Reflections reflections, ClassLoader classLoader, String workingPackage)
+	private <D> List<String> getPluginProcessNames(Reflections reflections, Class<D> processPluginDefinitionType,
+			ClassLoader classLoader, String workingPackage,
+			BiFunction<D, ClassLoader, List<String>> definitionToProcessNames)
 	{
-		List<Class<? extends ProcessPluginDefinition>> pluginDefinitionClasses = new ArrayList<>(
-				reflections.getSubTypesOf(ProcessPluginDefinition.class));
+		List<Provider<?>> definitions = ServiceLoader.load(processPluginDefinitionType, classLoader).stream()
+				.collect(Collectors.toList());
 
-		if (pluginDefinitionClasses.size() < 1)
+		if (definitions.size() < 1)
 		{
 			logger.warn("No ProcessPluginDefinitions found in package {}", workingPackage);
 			return List.of();
 		}
 
-		if (pluginDefinitionClasses.size() > 1)
-			logger.warn("Found {} ProcessPluginDefinitions ({}) in package {}, using {}",
-					pluginDefinitionClasses.size(), pluginDefinitionClasses, workingPackage,
-					pluginDefinitionClasses.get(0).getName());
+		if (definitions.size() > 1)
+			logger.warn("Found {} ProcessPluginDefinitions ({}) in package {}, using {}", definitions.size(),
+					definitions.stream().map(Provider::type).map(Class::getName).toList(), workingPackage,
+					definitions.get(0).type().getName());
 
 		try
 		{
-			ProcessPluginDefinition processPluginDefinition = pluginDefinitionClasses.get(0).getConstructor()
-					.newInstance();
-
-			return processPluginDefinition.getProcessModels().stream().map(f -> getProcessName(classLoader, f))
-					.filter(Optional::isPresent).map(Optional::get).collect(toList());
+			@SuppressWarnings("unchecked")
+			List<String> processNames = definitionToProcessNames.apply((D) definitions.get(0).get(), classLoader);
+			return processNames;
 		}
 		catch (Exception e)
 		{
 			logger.error("Could not read process names from package {} and ProcessPluginDefinition with name {}: {} {}",
-					workingPackage, pluginDefinitionClasses.get(0).getName(), e.getClass().getSimpleName(),
-					e.getMessage());
+					workingPackage, definitions.get(0).type().getName(), e.getClass().getSimpleName(), e.getMessage());
 			return List.of();
 		}
 	}
 
-	private Optional<String> getProcessName(ClassLoader classLoader, String bpmnFile)
+	private List<String> getV1ProcessNames(dev.dsf.bpe.v1.ProcessPluginDefinition definition, ClassLoader classLoader)
 	{
-		try (InputStream resource = classLoader.getResourceAsStream(bpmnFile))
+		return definition.getProcessModels().stream().map(getProcessName(classLoader)).flatMap(Optional::stream)
+				.toList();
+	}
+
+	private List<String> getV2ProcessNames(dev.dsf.bpe.v2.ProcessPluginDefinition definition, ClassLoader classLoader)
+	{
+		return definition.getProcessModels().stream().map(getProcessName(classLoader)).flatMap(Optional::stream)
+				.toList();
+	}
+
+	private Function<String, Optional<String>> getProcessName(ClassLoader classLoader)
+	{
+		return bpmnFile ->
 		{
-			return Bpmn.readModelFromStream(resource).getModelElementsByType(Process.class).stream()
-					.map(BaseElement::getId).findFirst();
-		}
-		catch (Exception exception)
-		{
-			logger.warn("Could not read process name from resource file {}: {}", bpmnFile, exception.getMessage());
-			return Optional.empty();
-		}
+			try (InputStream resource = classLoader.getResourceAsStream(bpmnFile))
+			{
+				return Bpmn.readModelFromStream(resource).getModelElementsByType(Process.class).stream()
+						.map(BaseElement::getId).findFirst();
+			}
+			catch (Exception exception)
+			{
+				logger.warn("Could not read process name from resource file {}: {}", bpmnFile, exception.getMessage());
+				return Optional.empty();
+			}
+		};
 	}
 
 	private void moveExistingToBackup(Path file)
@@ -200,6 +232,7 @@ public class DocumentationGenerator extends AbstractMojo
 				.map(DocumentationEntry::value)::iterator;
 		try
 		{
+			logger.debug("Writing {} entrie{} ...", fields.size(), fields.size() != 1 ? "s" : "");
 			Files.write(file, entries, StandardCharsets.UTF_8, StandardOpenOption.CREATE);
 		}
 		catch (IOException e)
@@ -209,20 +242,44 @@ public class DocumentationGenerator extends AbstractMojo
 		}
 	}
 
-	private Function<Field, DocumentationEntry> processDocumentationGenerator(List<String> pluginProcessNames)
+	private static record ProcessDocumentation(boolean required, String[] processNames, String description,
+			String recommendation, String example)
+	{
+		static ProcessDocumentation v1(Field field)
+		{
+			dev.dsf.bpe.v1.documentation.ProcessDocumentation documentation = field
+					.getAnnotation(dev.dsf.bpe.v1.documentation.ProcessDocumentation.class);
+
+			return new ProcessDocumentation(documentation.required(), documentation.processNames(),
+					documentation.description(), documentation.recommendation(), documentation.example());
+		}
+
+		static ProcessDocumentation v2(Field field)
+		{
+			dev.dsf.bpe.v2.documentation.ProcessDocumentation documentation = field
+					.getAnnotation(dev.dsf.bpe.v2.documentation.ProcessDocumentation.class);
+
+			return new ProcessDocumentation(documentation.required(), documentation.processNames(),
+					documentation.description(), documentation.recommendation(), documentation.example());
+		}
+	}
+
+	private Function<Field, DocumentationEntry> processDocumentationGenerator(List<String> pluginProcessNames,
+			Function<Field, ProcessDocumentation> getProcessDocumentation)
 	{
 		return field ->
 		{
-			ProcessDocumentation documentation = field.getAnnotation(ProcessDocumentation.class);
+			ProcessDocumentation documentation = getProcessDocumentation.apply(field);
 			Value value = field.getAnnotation(Value.class);
 
-			String[] valueSplit = getValueDefaultArray(value);
+			PropertyNameAndDefaultValue propertyNameAndDefaultValue = getPropertyNameAndDefaultValue(value);
+			String propertyName = propertyNameAndDefaultValue.propertyName();
+			String devalueValue = propertyNameAndDefaultValue.defaultValue();
 
-			String initialProperty = valueSplit.length > 0 ? valueSplit[0] : "";
-			String property = getDocumentationString("Property", initialProperty);
+			String propertyString = getDocumentationString("Property", propertyName);
 
-			String initialEnvironment = initialProperty.replace(".", "_").toUpperCase();
-			String environment = initialProperty.endsWith(".password") || initialProperty.endsWith(".secret")
+			String initialEnvironment = propertyName.replace(".", "_").toUpperCase();
+			String environment = propertyName.endsWith(".password") || propertyName.endsWith(".secret")
 					? String.format("%s or %s_FILE", initialEnvironment, initialEnvironment)
 					: initialEnvironment;
 
@@ -236,16 +293,53 @@ public class DocumentationGenerator extends AbstractMojo
 			String recommendation = getDocumentationString("Recommendation", documentation.recommendation());
 			String example = getDocumentationStringMonospace("Example", documentation.example());
 
+			String defaultValueString = devalueValue != null && devalueValue.length() > 1
+					&& !"#{null}".equals(devalueValue) ? getDocumentationStringMonospace("Default", devalueValue) : "";
 
-			String defaultValue = valueSplit.length > 1 && !"null".equals(valueSplit[1])
-					? getDocumentationStringMonospace("Default", valueSplit[1])
-					: "";
-
-			return new DocumentationEntry(initialProperty,
-					String.format("### %s%n%s%s%s%s%s%s%s%n", environment, property, required, processes, description,
-							recommendation, example, defaultValue).replace(ENV_VARIABLE_PLACEHOLDER, initialEnvironment)
-							.replace(PROPERTY_NAME_PLACEHOLDER, initialProperty));
+			return new DocumentationEntry(propertyName,
+					String.format("### %s%n%s%s%s%s%s%s%s%n", environment, propertyString, required, processes,
+							description, recommendation, example, defaultValueString)
+							.replace(ENV_VARIABLE_PLACEHOLDER, initialEnvironment)
+							.replace(PROPERTY_NAME_PLACEHOLDER, propertyName));
 		};
+	}
+
+	private static record PropertyNameAndDefaultValue(String propertyName, String defaultValue)
+	{
+	}
+
+	private PropertyNameAndDefaultValue getPropertyNameAndDefaultValue(Value value)
+	{
+		Matcher matcher1 = DEFAULT_VALUE_PATTERN_1.matcher(value.value());
+
+		if (matcher1.matches())
+		{
+			Matcher matcher2 = DEFAULT_VALUE_PATTERN_2.matcher(matcher1.group(1));
+			if (matcher2.matches())
+			{
+				String g = matcher2.group(1);
+				int i = g.indexOf(":");
+				if (i >= 0)
+					return new PropertyNameAndDefaultValue(g.substring(0, i), g.substring(i + 1));
+				else
+					return new PropertyNameAndDefaultValue(g, null);
+			}
+		}
+		else
+		{
+			Matcher matcher2 = DEFAULT_VALUE_PATTERN_2.matcher(value.value());
+			if (matcher2.matches())
+			{
+				String g = matcher2.group(1);
+				int i = g.indexOf(":");
+				if (i >= 0)
+					return new PropertyNameAndDefaultValue(g.substring(0, i), g.substring(i + 1));
+				else
+					return new PropertyNameAndDefaultValue(g, null);
+			}
+		}
+
+		return new PropertyNameAndDefaultValue(value.value(), null);
 	}
 
 	private Function<Field, DocumentationEntry> dsfDocumentationGenerator()
@@ -255,13 +349,14 @@ public class DocumentationGenerator extends AbstractMojo
 			Documentation documentation = field.getAnnotation(Documentation.class);
 			Value value = field.getAnnotation(Value.class);
 
-			String[] valueSplit = getValueDefaultArray(value);
+			PropertyNameAndDefaultValue propertyNameAndDefaultValue = getPropertyNameAndDefaultValue(value);
+			String propertyName = propertyNameAndDefaultValue.propertyName();
+			String devalueValue = propertyNameAndDefaultValue.defaultValue();
 
-			String initialProperty = valueSplit.length > 0 ? valueSplit[0] : "";
-			String property = getDocumentationString("Property", initialProperty);
+			String propertyString = getDocumentationString("Property", propertyName);
 
-			String initialEnvironment = initialProperty.replace(".", "_").toUpperCase();
-			String environment = initialProperty.endsWith(".password") || initialProperty.endsWith(".secret")
+			String initialEnvironment = propertyName.replace(".", "_").toUpperCase();
+			String environment = propertyName.endsWith(".password") || propertyName.endsWith(".secret")
 					? String.format("%s or %s_FILE", initialEnvironment, initialEnvironment)
 					: initialEnvironment;
 
@@ -271,28 +366,15 @@ public class DocumentationGenerator extends AbstractMojo
 			String recommendation = getDocumentationString("Recommendation", documentation.recommendation());
 			String example = getDocumentationStringMonospace("Example", documentation.example());
 
-			String defaultValue = valueSplit.length > 1 && !"null".equals(valueSplit[1])
-					? getDocumentationStringMonospace("Default", valueSplit[1])
-					: "";
+			String defaultValueString = devalueValue != null && devalueValue.length() > 1
+					&& !"#{null}".equals(devalueValue) ? getDocumentationStringMonospace("Default", devalueValue) : "";
 
-			return new DocumentationEntry(initialProperty,
-					String.format("### %s%n%s%s%s%s%s%s%n", environment, property, required, description,
-							recommendation, example, defaultValue).replace(ENV_VARIABLE_PLACEHOLDER, initialEnvironment)
-							.replace(PROPERTY_NAME_PLACEHOLDER, initialProperty));
+			return new DocumentationEntry(propertyName,
+					String.format("### %s%n%s%s%s%s%s%s%n", environment, propertyString, required, description,
+							recommendation, example, defaultValueString)
+							.replace(ENV_VARIABLE_PLACEHOLDER, initialEnvironment)
+							.replace(PROPERTY_NAME_PLACEHOLDER, propertyName));
 		};
-	}
-
-	private String[] getValueDefaultArray(Value value)
-	{
-		if (value == null)
-			return new String[] {};
-
-		String valueString = value.value();
-
-		if (valueString.startsWith("#{'"))
-			valueString = valueString.substring(valueString.indexOf("#{'") + 4, valueString.indexOf("}'"));
-
-		return valueString.replaceAll("\\$", "").replace("#", "").replace("{", "").replace("}", "").split(":");
 	}
 
 	private String getDocumentationStringMonospace(String title, String value)
