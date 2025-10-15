d54faddecc012f5bd89d5f123d07c972887f927f
Hauke Hund <hauke.hund@hs-heilbronn.de>
2025-10-15 17:31:47 +0200

    not null and not empty checks
    - Camunda engine requires code and message to be non null and non empty

--- dsf-bpe/dsf-bpe-process-api-v2/src/main/java/dev/dsf/bpe/v2/error/ErrorBoundaryEvent.java
+++ dsf-bpe/dsf-bpe-process-api-v2/src/main/java/dev/dsf/bpe/v2/error/ErrorBoundaryEvent.java
@@ -1,5 +1,7 @@
 package dev.dsf.bpe.v2.error;
 
+import java.util.Objects;
+
 public class ErrorBoundaryEvent extends RuntimeException
 {
 	private static final long serialVersionUID = 3161271266680097207L;
@@ -7,10 +9,21 @@ public class ErrorBoundaryEvent extends RuntimeException
 	private final String errorCode;
 	private final String errorMessage;
 
+	/**
+	 * @param errorCode
+	 *            not <code>null</code>, not empty
+	 * @param errorMessage
+	 *            not <code>null</code>, not empty
+	 */
 	public ErrorBoundaryEvent(String errorCode, String errorMessage)
 	{
-		this.errorCode = errorCode;
-		this.errorMessage = errorMessage;
+		this.errorCode = Objects.requireNonNull(errorCode, "errorCode");
+		this.errorMessage = Objects.requireNonNull(errorMessage, "errorMessage");
+
+		if (errorCode.isEmpty())
+			throw new IllegalArgumentException("errorCode empty");
+		if (errorMessage.isEmpty())
+			throw new IllegalArgumentException("errorMessage empty");
 	}
 
 	public String getErrorCode()
