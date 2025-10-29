ff36ddb3fba843df59567ca669c15d8fed77d291
Hauke Hund <hauke.hund@hs-heilbronn.de>
2025-10-29 04:32:22 +0100

    data migrations jobs to fix task profiles and questionnaire resources
    - QuestionnairesMigrationJob removes version prefix from profiles and
    sets "required" property for items with link-ids "business-key" and
    "user-task-id" to true else to false if Questionnaire had old 1.0.0
    profile
    - StructureDefinitionTaskProfileMigrationJob modifies baseDefinition
    (task-base -> task), adds structure-definition profile and generates new
    StructureDefinition snapshots
    - both migration jobs emit resource updated events
    - New methods in QuestionnaireDao and StructureDefinitionDao to simplify
    migration job implementation

--- dsf-fhir/dsf-fhir-server/src/main/java/dev/dsf/fhir/dao/QuestionnaireDao.java
+++ dsf-fhir/dsf-fhir-server/src/main/java/dev/dsf/fhir/dao/QuestionnaireDao.java
@@ -1,7 +1,12 @@
 package dev.dsf.fhir.dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.hl7.fhir.r4.model.Questionnaire;
 
 public interface QuestionnaireDao extends ResourceDao<Questionnaire>, ReadByUrlDao<Questionnaire>
 {
+	List<Questionnaire> readAllByProfileWithTransaction(Connection connection, String profile) throws SQLException;
 }
