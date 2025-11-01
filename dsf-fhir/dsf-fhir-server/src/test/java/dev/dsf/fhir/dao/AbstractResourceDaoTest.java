2ddc50d25aa83eec174871217819fedb6dd63633
Hauke Hund <hauke.hund@hs-heilbronn.de>
2025-11-01 23:15:53 +0100

    PostgreSQL 15 to 18 upgrade
    Warning: PostgreSQL 18 requires different mount point.
    
    PostgreSQL 15: /var/lib/postgresql/data
    PostgreSQL 18: /var/lib/postgresql

--- dsf-fhir/dsf-fhir-server/src/test/java/dev/dsf/fhir/dao/AbstractResourceDaoTest.java
+++ dsf-fhir/dsf-fhir-server/src/test/java/dev/dsf/fhir/dao/AbstractResourceDaoTest.java
@@ -48,7 +48,7 @@ public abstract class AbstractResourceDaoTest<D extends Resource, C extends Reso
 
 	@ClassRule
 	public static final PostgreSqlContainerLiquibaseTemplateClassRule liquibaseRule = new PostgreSqlContainerLiquibaseTemplateClassRule(
-			DockerImageName.parse("postgres:15"), ROOT_USER, "fhir", "fhir_template", CHANGE_LOG_FILE,
+			DockerImageName.parse("postgres:18"), ROOT_USER, "fhir", "fhir_template", CHANGE_LOG_FILE,
 			CHANGE_LOG_PARAMETERS, true);
 
 	@Rule
