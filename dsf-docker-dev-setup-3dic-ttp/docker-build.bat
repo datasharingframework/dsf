e0a895e4b733e5207f6ec9bc89c517f01f3bc351
Filiz Kluba <Filiz.Kluba@unimedizin-mainz.de>
2026-06-02 09:09:36 +0200

    revert previos changes

--- dsf-docker-dev-setup-3dic-ttp/docker-build.bat
+++ dsf-docker-dev-setup-3dic-ttp/docker-build.bat
@@ -17,7 +17,7 @@
 @echo off
 
 echo datasharingframework/bpe ...
-docker build --pull -t datasharingframework/bpe ..\..\dsf-bpe\dsf-bpe-server-jetty\docker
+docker build --pull -t datasharingframework/bpe ..\dsf-bpe\dsf-bpe-server-jetty\docker
 
 echo datasharingframework/fhir ...
-docker build --pull -t datasharingframework/fhir ..\..\dsf-fhir\dsf-fhir-server-jetty\docker
+docker build --pull -t datasharingframework/fhir ..\dsf-fhir\dsf-fhir-server-jetty\docker
