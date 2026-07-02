31ca79d0b1fe002ec0d2486edd0888b6d2220c6c
Mathias Rühle <math2306@hotmail.com>
2026-07-03 00:09:39 +0200

    URL-encode client id and client secret for Basic Auth
    RFC 6749 requires the client id and client secret to be url encoded
    first when using HTTP Basic authentication scheme.
    
    see https://datatracker.ietf.org/doc/html/rfc6749#section-2.3.1

--- dsf-bpe/dsf-bpe-server/src/test/java/dev/dsf/bpe/client/oidc/OidcClientJerseyTest.java
+++ dsf-bpe/dsf-bpe-server/src/test/java/dev/dsf/bpe/client/oidc/OidcClientJerseyTest.java
@@ -15,22 +15,51 @@
  */
 package dev.dsf.bpe.client.oidc;
 
-import static org.junit.Assert.assertNotNull;
+import static java.nio.charset.StandardCharsets.US_ASCII;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URLEncoder;
 import java.nio.file.Paths;
 import java.security.KeyStore;
 import java.time.Duration;
+import java.util.Base64;
+import java.util.List;
+import java.util.Set;
 
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExternalResource;
 
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsServer;
+
+import de.hsheilbronn.mi.utils.crypto.context.SSLContextFactory;
 import de.hsheilbronn.mi.utils.crypto.io.PemReader;
 import de.hsheilbronn.mi.utils.crypto.keystore.KeyStoreCreator;
+import dev.dsf.bpe.api.client.oidc.OidcClientException;
+import dev.dsf.bpe.integration.X509Certificates;
+import dev.dsf.common.oidc.Jwks;
+import dev.dsf.common.oidc.OidcConfiguration;
+import jakarta.ws.rs.core.HttpHeaders;
 
-@Ignore
-// Needs keycloak service from 3dic-ttp dev setup, "Service accounts roles" needs to be activated for dic1-fhir client
 public class OidcClientJerseyTest
 {
+	@Rule
+	public final X509Certificates certificates = new X509Certificates();
+
+	@Rule
+	public final TokenEndpointServerResource tokenEndpointServerResource = new TokenEndpointServerResource();
+
+	// Needs keycloak service from 3dic-ttp dev setup, "Service accounts roles" needs to be activated for dic1-fhir
+	// client
+	@Ignore
 	@Test
 	public void getAccessToken() throws Exception
 	{
@@ -44,5 +73,97 @@ public class OidcClientJerseyTest
 
 		char[] accessToken = client.asOidcClientWithDecodedJwt().getAccessToken();
 		assertNotNull(accessToken);
+	}
+
+	@Test
+	public void basicAuthorizationClientIdAndSecretShouldBeUrlEncoded() throws Exception
+	{
+		var clientId = "client/id:+";
+		var clientSecret = "client/secret:+";
+		var expectedAuthorizationHeaderValue = "Basic ".concat(clientId.transform(id -> URLEncoder.encode(id, US_ASCII))
+				.concat(":").concat(clientSecret.transform(secret -> URLEncoder.encode(secret, US_ASCII)))
+				.transform(s -> Base64.getEncoder().encodeToString(s.getBytes(US_ASCII))));
+		var client = new OidcClientJersey("https://localhost:" + tokenEndpointServerResource.getPort(),
+				"/.well-known/openid-configuration", clientId, clientSecret.toCharArray(),
+				certificates.getFhirServerCertificate().trustStore(), null, null, null, null, null, "Test Client",
+				Duration.ofSeconds(10), Duration.ofSeconds(5), true, Duration.ofSeconds(10), null, false);
+		var config = new OidcConfiguration("https://localhost:" + tokenEndpointServerResource.getPort(),
+				"https://localhost:" + tokenEndpointServerResource.getPort() + "/token", "https://localhost/jwks",
+				Set.of("client_credentials"));
+
+		// token is intentionally invalid as it is not the focus of this test
+		// and to simplify test code
+		assertThrows(OidcClientException.class, () -> client.getAccessTokenDecoded(config, new Jwks(List.of())));
+
+		assertEquals(expectedAuthorizationHeaderValue, tokenEndpointServerResource.getAuthorizationHeaderValue());
+	}
+
+	private class TokenEndpointServerResource extends ExternalResource
+	{
+		private HttpsServer server;
+		private String authorizationHeaderValue;
+
+		@Override
+		protected void before() throws Throwable
+		{
+			// Server will be created lazily when getPort() is first called
+		}
+
+		@Override
+		protected void after()
+		{
+			if (server != null)
+			{
+				server.stop(0);
+			}
+		}
+
+		int getPort() throws Exception
+		{
+			if (server == null)
+			{
+				server = createServer();
+				server.start();
+			}
+			return server.getAddress().getPort();
+		}
+
+		String getAuthorizationHeaderValue()
+		{
+			return authorizationHeaderValue;
+		}
+
+		private HttpsServer createServer() throws Exception
+		{
+			var newServer = HttpsServer.create(new InetSocketAddress("localhost", 0), 0);
+			var serverCertificate = certificates.getFhirServerCertificate();
+
+			var keyStorePassword = "server-password".toCharArray();
+			KeyStore keyStore = KeyStoreCreator.jksForPrivateKeyAndCertificateChain(serverCertificate.privateKey(),
+					keyStorePassword, serverCertificate.certificate(), serverCertificate.caCertificate());
+
+			newServer.setHttpsConfigurator(
+					new HttpsConfigurator(SSLContextFactory.createSSLContext(null, keyStore, keyStorePassword, "TLS")));
+
+			newServer.createContext("/token", exchange ->
+			{
+				authorizationHeaderValue = exchange.getRequestHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+				byte[] response = "{\"access_token\":\"invalid\",\"expires_in\":300}".getBytes(US_ASCII);
+				exchange.getResponseHeaders().set(HttpHeaders.CONTENT_TYPE, "application/json");
+				exchange.sendResponseHeaders(200, response.length);
+				try (OutputStream out = exchange.getResponseBody())
+				{
+					out.write(response);
+				}
+				catch (IOException e)
+				{
+					throw new RuntimeException(e);
+				}
+				exchange.close();
+			});
+
+			return newServer;
+		}
 	}
 }
