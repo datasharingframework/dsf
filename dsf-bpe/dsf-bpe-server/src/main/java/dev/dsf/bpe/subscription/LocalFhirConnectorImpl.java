a278e2e821e04f7db4a011abd85e62a4638163e9
Hauke Hund <hauke.hund@hs-heilbronn.de>
2026-08-04 21:21:24 +0200

    typo fix Unsupportet -> Unsupported

--- dsf-bpe/dsf-bpe-server/src/main/java/dev/dsf/bpe/subscription/LocalFhirConnectorImpl.java
+++ dsf-bpe/dsf-bpe-server/src/main/java/dev/dsf/bpe/subscription/LocalFhirConnectorImpl.java
@@ -300,7 +300,7 @@ public class LocalFhirConnectorImpl<R extends Resource> implements LocalFhirConn
 			case Constants.CT_FHIR_JSON, Constants.CT_FHIR_JSON_NEW -> EventType.JSON;
 			case Constants.CT_FHIR_XML, Constants.CT_FHIR_XML_NEW -> EventType.XML;
 
-			default -> throw new RuntimeException("Unsupportet subscription.payload " + payload);
+			default -> throw new RuntimeException("Unsupported subscription.payload " + payload);
 		};
 	}
 
