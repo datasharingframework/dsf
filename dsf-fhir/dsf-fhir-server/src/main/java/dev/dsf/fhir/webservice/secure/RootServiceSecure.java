5e8a97f9631ab2cd10625921ed1259495c236fb5
Hauke Hund <hauke.hund@hs-heilbronn.de>
2026-08-25 19:16:03 +0200

    typo fixes generaly -> generally, individualy -> individually

--- dsf-fhir/dsf-fhir-server/src/main/java/dev/dsf/fhir/webservice/secure/RootServiceSecure.java
+++ dsf-fhir/dsf-fhir-server/src/main/java/dev/dsf/fhir/webservice/secure/RootServiceSecure.java
@@ -86,8 +86,8 @@ public class RootServiceSecure extends AbstractServiceSecure<RootService> implem
 		if (BundleType.BATCH.equals(bundle.getType()) || BundleType.TRANSACTION.equals(bundle.getType()))
 		{
 			logger.info(
-					"Handling of batch or transaction bundles generaly allowed for all, entries will be individualy evaluated");
-			return Optional.of("Allowed for all, entries individualy evaluated");
+					"Handling of batch or transaction bundles generally allowed for all, entries will be individually evaluated");
+			return Optional.of("Allowed for all, entries individually evaluated");
 		}
 		else
 		{
